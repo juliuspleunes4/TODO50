8b292c63e58e7ec3b6344ceb0a6c68470e4ee53b
Julius Pleunes <jjgpleunes@gmail.com>
2024-10-22 12:10:08 +0200

    Initial Commit

---
+++ TODO50/TODO50App.swift
@@ -0,0 +1,17 @@
+//
+//  TODO50App.swift
+//  TODO50
+//
+//  Created by Julius Pleunes on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TODO50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TODO50Tests/TODO50Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TODO50Tests.swift
+//  TODO50Tests
+//
+//  Created by Julius Pleunes on 22/10/2024.
+//
+
+import Testing
+@testable import TODO50
+
+struct TODO50Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
